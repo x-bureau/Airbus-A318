5a84e2a2ae53140f63a82962a3254897fa508ae5
FBI914-Admin <67609815+FBI914-Admin@users.noreply.github.com>
2020-08-01 16:13:06 -0400

    Added ECAM with N1, N2, and EGT
    EGT values not working yet

---
+++ plugins/SASLFree/data/modules/Custom Module/ecam.lua
@@ -0,0 +1,36 @@
+--A318 Created by X-Bureau--
+
+position = {1144, 622, 522, 522}
+size = {522, 522}
+
+--get datarefs
+local npercent = globalPropertyfa("sim/cockpit2/engine/indicators/N1_percent", 7)
+local n2percent = globalPropertyfa("sim/cockpit2/engine/indicators/N1_percent", 7)
+local EGT = globalPropertyfa("sim/flightmodel/engine/ENGN_EGT", 7)
+
+
+--fonts
+local AirbusFont = sasl.gl.loadFont("fonts/PanelFont.ttf")
+
+--colors
+local PFD_GREEN = {0.184, 0.733, 0.219, 1.0}
+local PFD_WHITE = {1.0, 1.0, 1.0, 1.0}
+local PFD_BLUE = {0.004, 1.0, 1.0, 1.0}
+local PFD_GREY = {0.25, 0.26, 0.26, 1.0}
+
+
+--custom functions
+
+local function draw_ENG_PAGE()
+    sasl.gl.drawText(AirbusFont, 205, 455, math.floor(get(npercent, 1)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+    sasl.gl.drawText(AirbusFont, 410, 455, math.floor(get(npercent, 2)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+    sasl.gl.drawText(AirbusFont, 190, 325, math.floor(get(n2percent, 1)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+    sasl.gl.drawText(AirbusFont, 390, 325, math.floor(get(n2percent, 2)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+    sasl.gl.drawText(AirbusFont, 215, 365, math.floor(get(EGT, 1)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+    sasl.gl.drawText(AirbusFont, 390, 365, math.floor(get(EGT, 2)), 21, false, false, TEXT_ALIGN_RIGHT, PFD_GREEN)
+end
+
+function draw()
+    draw_ENG_PAGE()
+    print(get(npercent, 1))
+end
