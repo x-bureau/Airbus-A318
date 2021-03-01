65f9301d113e96083b8a7fd976c833dda45bcf9b
OldGroof <39658757+OldGroof@users.noreply.github.com>
2021-03-01 19:38:00 +0000

    Updated main.lua to fix 'function update()'

--- plugins/SASLFree/data/modules/main.lua
+++ plugins/SASLFree/data/modules/main.lua
@@ -1,11 +1,12 @@
 sasl.options.setAircraftPanelRendering(true)
-sasl.options.set3DRendering(true)
+sasl.options.set3DRendering(false)
 sasl.options.setInteractivity(true)
 addSearchPath(moduleDirectory.."/images/")
 addSearchPath(moduleDirectory .. "/Custom Module/ATSAW/")
 addSearchPath(moduleDirectory .. "/Custom Module/ND/")
 addSearchPath(moduleDirectory .. "/Custom Module/ADIRS/")
 addSearchPath(moduleDirectory .. "/Custom Module/PFD/")
+addSearchPath(moduleDirectory .. "/Custom Module/systems/")
 
 size = {2048, 2048}
 
@@ -14,13 +15,26 @@ panelHeight3d = 2048
 
 components = {
   customdataref {};
-  systems {};
+  electrical {};
+  hydraulics {};
   ADIRS {};
   MCDU {};
-  PFD_CAPT {};
-  PFD_FRST {};
-  ND_CAPT {};
-  ND_FRST {};
+  PFD_CAPT {
+    fbo = true ,
+    fpsLimit = 29
+  };
+  PFD_FRST {
+    fbo = true ,
+    fpsLimit = 29
+  };
+  ND_CAPT {
+    fbo = true ,
+    fpsLimit = 29
+  };
+  ND_FRST {
+    fbo = true ,
+    fpsLimit = 29
+  };
   -- ATSAW {};
   ecam {};
   lower_ecam {};
