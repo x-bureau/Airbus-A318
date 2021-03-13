d04b0e698cfbc861c1b643ab15631230a1cee863
OldGroof <39658757+OldGroof@users.noreply.github.com>
2021-03-13 19:14:18 +0000

    Some ELEC stuff (May be broken until cockpit updates)
    - Updated DC BUS TIE Logic.
    - Added EXT PWR and APU GEN contact logic to favour EXT PWR if both are available.
    - Updated ELEC ECAM Page.

--- plugins/SASLFree/data/modules/Custom Module/elec.lua
+++ plugins/SASLFree/data/modules/Custom Module/elec.lua
@@ -85,6 +85,8 @@ dc_bus_ess = {
 
 bus_tie = createGlobalPropertyi("A318/systems/ELEC/busTieSwtch", 0)
 ac_ess_feed = createGlobalPropertyi("A318/systems/ELEC/ESSFeedSwtch", 0)
+apu_gen = createGlobalPropertyi("A318/systems/ELEC/APUGENSwtch", 1)
+ext_pow = createGlobalPropertyi("A318/systems/ELEC/EXTPWRSwtch", 0)
 
 contacts = {
     -- Batteries
@@ -220,7 +222,17 @@ function update()
         set(bat_2.amps, 0)
     end
 
-
+    -- AUXILIARY POWER BUTTON
+    if (get(apu_gen) == 1 and get(ext_pow) == 1) or (get(apu_gen) == 0 and get(ext_pow) == 1) then
+        set(contacts.AGC, 0)
+        set(contacts.EPC, 1)
+    elseif get(apu_gen) == 1 then
+        set(contacts.AGC, 1)
+        set(contacts.EPC, 0)
+    else
+        set(contacts.AGC, 0)
+        set(contacts.EPC, 0)
+    end
     -- BUS TIE LOGIC
     if get(bus_tie) == 0 then
         if get(contacts.GLC1) == 1 and get(gen_1.voltage) > 0 and get(contacts.GLC2) == 1 and get(gen_1.voltage) > 0 then
@@ -402,17 +414,17 @@ function update()
     end
 
     -- DC BUS TIE CONT LOGIC
-    if get(dc_bus_1.voltage) > 0 and get(contacts.TR1) == 1 then
+    if get(dc_bus_1.voltage) > 0 and get(contacts.TR1) == 1 and get(contacts.TR2) == 0 then
+        set(contacts.DCT1, 1)
+        set(contacts.DCT2, 1)
+    elseif get(dc_bus_2.voltage) > 0 and get(contacts.TR2) == 1 and get(contacts.TR1) == 0 then
+        set(contacts.DCT1, 1)
+        set(contacts.DCT2, 1)
+    elseif get(dc_bus_1.voltage) > 0 and get(contacts.TR1) == 1 then
         set(contacts.DCT1, 1)
         set(contacts.DCT2, 0)
-    elseif get(dc_bus_1.voltage) > 0 and get(contacts.TR1) == 0 then
-        set(contacts.DCT1, 1)
-        set(contacts.DCT2, 1)
     elseif get(dc_bus_2.voltage) > 0 and get(contacts.TR2) == 1 then
         set(contacts.DCT1, 0)
-        set(contacts.DCT2, 1)
-    elseif get(dc_bus_2.voltage) > 0 and get(contacts.TR2) == 0 then
-        set(contacts.DCT1, 1)
         set(contacts.DCT2, 1)
     else
         set(contacts.DCT1, 0)
