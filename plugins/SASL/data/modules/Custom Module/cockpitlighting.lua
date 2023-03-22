ff7a86919941b543b5ab477c2b6a984b58e63328
FBI914XB <67609815+FBI914XB@users.noreply.github.com>
2023-03-22 19:55:21 -0400

    Playing with lighting

---
+++ plugins/SASL/data/modules/Custom Module/cockpitlighting.lua
@@ -0,0 +1,5 @@
+-- Define Capt Reading Lights
+local captReadLight = createGlobalPropertyfa("A318/cockpit/lights/capt_reading", 9)
+
+-- Initialize Captain Reading Light to ON
+set(captReadLight, 1, 7)
